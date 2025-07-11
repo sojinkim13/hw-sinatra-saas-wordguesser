fad0850424a6dd38bb976bef0b269e31dbae97c8
Sojin Kim <sojin.kim@berkeley.edu>
2025-07-11 22:10:45 +0000

    finished all of the tests and pushing to my repo

--- app.rb
+++ app.rb
@@ -39,8 +39,14 @@ class WordGuesserApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    params[:guess].to_s[0]
+    letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    ret = @game.guess(letter)
+    if ret == ArgumentError
+      flash[:message] = "Invalid guess."
+    elsif ret == false
+      flash[:message] = "You have already used that letter."    
+    end
     redirect '/show'
   end
 
@@ -51,16 +57,35 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    state = @game.check_win_or_lose() 
+    if state == :win
+      redirect '/win'
+    elsif state == :lose
+      redirect '/lose'
+    else
+      erb :show
+    end 
   end
 
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    state = @game.check_win_or_lose() 
+    if state == :win
+      erb :win
+    else
+      redirect '/show'
+    end 
+    # erb :win # You may change/remove this line
   end
 
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    state = @game.check_win_or_lose() 
+    if state == :lose
+      erb :lose
+    else
+      redirect '/show'
+    end 
+    # erb :lose # You may change/remove this line
   end
 end
--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -2,11 +2,57 @@ class WordGuesserGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
+
+  attr_accessor :word, :guesses, :wrong_guesses
   # Get a word from remote "random word" service
 
-  def initialize(word)
+  def initialize(word, guesses='', wrong_guesses='')
     @word = word
+    @guesses = guesses
+    @wrong_guesses = wrong_guesses
+    @masked_word = '-'*word.length
   end
+
+  def guess(letter)
+    if letter == nil || letter.empty? || (/[a-z]/i =~ letter) == nil
+      return ArgumentError
+    end
+
+    letter = letter.downcase
+
+    if (@guesses.include? letter) || (@wrong_guesses.include? letter)
+      return false
+    end
+
+    if !(word.include? letter)
+      @wrong_guesses += letter
+      return true
+    end
+
+
+    @guesses += letter
+    while (i = @word.index(letter)) != nil 
+      @masked_word[i] = letter
+      @word[i] = "-"
+    end
+
+    return true
+  end
+
+  def word_with_guesses()
+    @masked_word
+  end
+
+  def check_win_or_lose()
+    if @wrong_guesses.length >= 7
+      return :lose
+    elsif @word == "-"*@word.length
+      return :win
+    else
+      return :play
+    end
+  end
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
